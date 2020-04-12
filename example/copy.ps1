034ebfe539d4aa44f1f953c2e1b17d2398ffccff
James Clarke <james@clarkezone.net>
2020-04-12 09:34:03 -0700

    Support release mode builds of Flutter Engine

--- example/copy.ps1
+++ example/copy.ps1
@@ -1,27 +1,45 @@
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll .\windows\flutter\ephemeral\.
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll.pdb .\windows\flutter\ephemeral\.
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll.exp .\windows\flutter\ephemeral\.
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll.lib .\windows\flutter\ephemeral\.
+#$sourcepath = '..\..\engine\src\out\host_debug_unopt\'
+$sourcepath = '..\..\engine\src\out\host_release\'
 
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll .\build\windows\x64\Debug\Runner\.
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll.pdb .\build\windows\x64\Debug\Runner\.
+$fdll = Join-Path -Path $sourcepath -ChildPath 'flutter_windows.dll'
+$fpdb = Join-Path -Path $sourcepath -ChildPath 'flutter_windows.dll.pdb'
+$fexp = Join-Path -Path $sourcepath -ChildPath 'flutter_windows.dll.exp'
+$flib = Join-Path -Path $sourcepath -ChildPath 'flutter_windows.dll.lib'
 
-Copy-Item -Recurse -Force ..\..\engine\src\out\host_debug_unopt\cpp_client_wrapper .\windows\flutter\ephemeral\.
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.h .\windows\flutter\ephemeral\.
+# win32
+copy-item $fdll .\windows\flutter\ephemeral\.
+copy-item $fpdb .\windows\flutter\ephemeral\.
+copy-item $fexp .\windows\flutter\ephemeral\.
+copy-item $flib .\windows\flutter\ephemeral\.
+
+# win32 runner
+copy-item $fdll .\build\windows\x64\Debug\Runner\.
+copy-item $fpdb .\build\windows\x64\Debug\Runner\.
+
+# uwp
+copy-item $flib .\windowsuwp\flutter\ephemeral\.
+copy-item $fdll .\windowsuwp\flutter\ephemeral\.
+copy-item $fpdb .\windowsuwp\flutter\ephemeral\.
+copy-item $fexp .\windowsuwp\flutter\ephemeral\.
+
+# uwp package
+copy-item $fdll .\windowsuwp\.
+copy-item $fpdb .\windowsuwp\.
+
+# uwp runner
+copy-item $fdll .\windowsuwp\x64\Debug\windowsuwp\AppX\.
+copy-item $fpdb .\windowsuwp\x64\Debug\windowsuwp\AppX\.
+#copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll.exp .\windowsuwp\x64\Debug\windowsuwp\AppX\.
 
 #copy the data into UWP
 Copy-Item -Recurse -Force .\build\windows\x64\Debug\Runner\data .\windowsuwp\x64\Debug\windowsuwp\AppX\Assets\.
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll .\windowsuwp\x64\Debug\windowsuwp\AppX\.
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll.pdb .\windowsuwp\x64\Debug\windowsuwp\AppX\.
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll.exp .\windowsuwp\x64\Debug\windowsuwp\AppX\.
 
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll.lib .\windowsuwp\flutter\ephemeral\.
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll .\windowsuwp\flutter\ephemeral\.
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll.pdb .\windowsuwp\flutter\ephemeral\.
-copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.dll.exp .\windowsuwp\flutter\ephemeral\.
-
+#source
+Copy-Item -Recurse -Force ..\..\engine\src\out\host_debug_unopt\cpp_client_wrapper .\windows\flutter\ephemeral\.
+copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.h .\windows\flutter\ephemeral\.
 Copy-Item -Recurse -Force ..\..\engine\src\out\host_debug_unopt\cpp_client_wrapper .\windowsuwp\flutter\ephemeral\.
 copy-item ..\..\engine\src\out\host_debug_unopt\flutter_windows.h .\windowsuwp\flutter\ephemeral\.
 copy-item ..\..\engine\src\out\host_debug_unopt\flutter_export.h .\windowsuwp\flutter\ephemeral\.
 copy-item ..\..\engine\src\out\host_debug_unopt\flutter_messenger.h .\windowsuwp\flutter\ephemeral\.
 copy-item ..\..\engine\src\out\host_debug_unopt\flutter_plugin_registrar.h .\windowsuwp\flutter\ephemeral\.
+
